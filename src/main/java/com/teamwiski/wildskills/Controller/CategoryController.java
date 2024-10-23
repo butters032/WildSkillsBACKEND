4c73a3b10db6baf286d3f3581635cfdcbce92edc
butters023 <143509812+butters023@users.noreply.github.com>
2024-10-23 17:38:47 +0800

    Cross Origin

--- src/main/java/com/teamwiski/wildskills/Controller/CategoryController.java
+++ src/main/java/com/teamwiski/wildskills/Controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.teamwiski.wildskills.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.teamwiski.wildskills.Service.CategoryService;
 
 @RestController
 @RequestMapping(method=RequestMethod.GET,path="/api/category")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
     @Autowired
     CategoryService cserv;
