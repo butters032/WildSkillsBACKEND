a02a57db8325f48ada836dfde7e9bd2cf8fff4ed
butters023 <143509812+butters023@users.noreply.github.com>
2024-10-23 14:46:20 +0800

    Project Creation

---
+++ src/main/java/com/teamwiski/wildskills/WildskillsApplication.java
@@ -0,0 +1,13 @@
+package com.teamwiski.wildskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamwiski/wildskills/WildskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwiski.wildskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
