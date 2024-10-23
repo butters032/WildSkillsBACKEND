969fb0a1aaacd5c4cdbb2fa902055d6a84d00661
butters023 <143509812+butters023@users.noreply.github.com>
2024-10-23 15:51:03 +0800

    Jake Update

--- src/main/java/com/teamwiski/wildskills/Controller/SkillOfferingController.java
+++ src/main/java/com/teamwiski/wildskills/Controller/SkillOfferingController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.teamwiski.wildskills.Entity.CategoryEntity;
 import com.teamwiski.wildskills.Entity.SkillOfferingEntity;
 import com.teamwiski.wildskills.Service.SkillOfferingService;
 
