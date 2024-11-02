36676058598cb61033cb38ade43026b80a97ca61
butters023 <143509812+butters023@users.noreply.github.com>
2024-11-02 21:08:06 +0800

    Modified Jake Entities, Relationship Added

--- src/main/java/com/teamwiski/wildskills/Entity/CategoryEntity.java
+++ src/main/java/com/teamwiski/wildskills/Entity/CategoryEntity.java
@@ -1,9 +1,14 @@
 package com.teamwiski.wildskills.Entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,26 +19,42 @@ public class CategoryEntity {
     private int categoryId;
 
     private String name;
-  
-    public CategoryEntity(){
+
+    @OneToMany(fetch=FetchType.LAZY,mappedBy="category",
+                cascade=CascadeType.ALL)
+    private List<SkillOfferingEntity> skillOfferings; 
+
+    public CategoryEntity() {
         super();
-    }
-    public CategoryEntity(int categoryId, String name){
-        super();
-        this.categoryId=categoryId;
-        this.name=name;
-    }
-    public int getCategoryId(){
-        return categoryId;
-    }
-    public void setCategoryId(int categoryId){
-        this.categoryId=categoryId;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
     }
 
+    public CategoryEntity(int categoryId, String name) {
+        super();
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SkillOfferingEntity> getSkillOfferings() {
+        return skillOfferings;
+    }
+
+    public void setSkillOfferings(List<SkillOfferingEntity> skillOfferings) {
+        this.skillOfferings = skillOfferings;
+    }
 }
--- src/main/java/com/teamwiski/wildskills/Entity/SkillOfferingEntity.java
+++ src/main/java/com/teamwiski/wildskills/Entity/SkillOfferingEntity.java
@@ -1,9 +1,12 @@
 package com.teamwiski.wildskills.Entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,39 +19,72 @@ public class SkillOfferingEntity {
     private String title;
     private boolean isActive;
     private String description;
-    public SkillOfferingEntity(){
+    private String skills;  
+
+    @ManyToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="categoryID")
+    
+    private CategoryEntity category; 
+
+    public SkillOfferingEntity() {
         super();
-    }
-    public SkillOfferingEntity(int skillOfferingId, String title, boolean isActive, String description){
-        super();
-        this.skillOfferingId=skillOfferingId;
-        this.title=title;
-        this.isActive=isActive;
-        this.description=description;
-    }
-    public int getSkillOfferingId(){
-        return skillOfferingId;
-    }
-    public void setSkillOfferingId(int skillOfferingId){
-        this.skillOfferingId=skillOfferingId;
-    }
-    public String getTitle(){
-        return title;
-    }
-    public void setTitle(String title){
-        this.title=title;
-    }
-    public boolean getIsActive(){
-        return isActive;
-    }
-    public void setIsActive(boolean isActive){
-        this.isActive=isActive;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public void setDescription(String description){
-        this.description=description;
     }
 
+    public SkillOfferingEntity(int skillOfferingId, String title, boolean isActive, String description, String skills, CategoryEntity category) {
+        super();
+        this.skillOfferingId = skillOfferingId;
+        this.title = title;
+        this.isActive = isActive;
+        this.description = description;
+        this.skills = skills;
+        this.category = category;
+    }
+
+    public int getSkillOfferingId() {
+        return skillOfferingId;
+    }
+
+    public void setSkillOfferingId(int skillOfferingId) {
+        this.skillOfferingId = skillOfferingId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public CategoryEntity getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEntity category) {
+        this.category = category;
+    }
 }
