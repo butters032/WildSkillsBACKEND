0cfc39bf92e611b2e7416bf515859c5d6aa0e1ab
Abel-O2 <samuel.abrenica01@gmail.com>
2025-02-16 22:28:13 +0800

    Fixed Student Entity bugs

--- src/main/java/com/teamwiski/wildskills/Config/WebSocketConfig.java
+++ src/main/java/com/teamwiski/wildskills/Config/WebSocketConfig.java
@@ -1,11 +1,18 @@
 package com.teamwiski.wildskills.Config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -14,8 +21,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         // Configure message broker with prefix
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/user");
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix(("/user"));
     }
 
     @Override
@@ -23,4 +31,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // WebSocket endpoint for connecting clients
         registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
     }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+
+        return false;
+    }
+
 }
